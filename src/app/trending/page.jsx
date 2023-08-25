5da2425d3abcdd387e7a91e04ba34616411daa9b
Aditya Bhatt <bhattaditya92@gmail.com>
2023-08-25 12:19:26 +0530

    test

--- src/app/trending/page.jsx
+++ src/app/trending/page.jsx
@@ -1,6 +1,6 @@
 
 import { Navbar } from "@/app/components/Navbar"
-import styles from '../../styles/trending.module.css'
+import styles from '../styles/trending.module.css'
 import Image from 'next/image'
 import Link from "next/link"
 import { Footer } from "@/app/components/Footer"
@@ -9,7 +9,7 @@ import { Footer } from "@/app/components/Footer"
 
 
 
-const getGenres = async(param) => {
+const getTrending = async() => {
 
   
   const date = new Date();
@@ -17,7 +17,7 @@ const getGenres = async(param) => {
   const animeYear = year - 5;
   
   // const res = await fetch(`https://api.jikan.moe/v4/anime?genres=${param}&min_score=7&start_date=${animeYear}-01-01&sort=desc&sfw`)
-  const res = await fetch(`https://api.jikan.moe/v4/anime?genres=${param}&min_score=7&start_date=${animeYear}-01-01&sort=desc`)
+  const res = await fetch(`https://api.jikan.moe/v4/anime?&min_score=7&start_date=${animeYear}-01-01&sort=desc`)
   // const res = await fetch('https://api.jikan.moe/v4/recommendations/anime')
   
   const data = await res.json()
@@ -30,12 +30,10 @@ const getGenres = async(param) => {
 
 
 
-export  default async  function page({params}){
+export  default async  function page(){
 
 
-  const param = params?.id
-   let page = 1
-    const data = await getGenres(param, page)
+    const data = await getTrending()
     
  
     // console.log(data[0])
@@ -60,7 +58,6 @@ let end = data.length;
    <Image src={data[start]?.images?.webp?.large_image_url} height={500} width={500}  className={styles.mainImages} alt={data[start]?.title}/>
    {/* <div className= {styles.categoriesTopAnimeOneInfo}> */}
 
-  <h2 className={styles.categoriesTopTitle}>{data[start]?.title}</h2>
 
    {/* </div> */}
    
@@ -74,7 +71,7 @@ let end = data.length;
    
    {/* <div className= {styles.categoriesTopAnimeOneInfo}> */}
 
-  <h2 className={styles.categoriesTopTitle}>{data[start + 1]?.title}</h2>
+
    <Image src={data[start + 1]?.images?.webp.large_image_url} height={500} width={500} className={styles.mainImages} alt={data[start]?.title} />
 
    {/* </div> */}
@@ -91,7 +88,6 @@ let end = data.length;
    <Image src={data[start + 2]?.images?.webp?.large_image_url} height={500} width={500} className={styles.mainImages} alt={data[start]?.title}/>
    {/* <div className= {styles.categoriesTopAnimeOneInfo}> */}
 
-  <h2 className={styles.categoriesTopTitle}>{data[start + 2]?.title}</h2>
 
    {/* </div> */}
    
@@ -100,19 +96,7 @@ let end = data.length;
 
 </div>
 
-<div className={styles.categoriesAnimes}>
- 
- {
-  data.splice(start + 3 , end).map(items => (
-    <Link href={`/anime/${items?.mal_id}`} style={{textDecoration : 'none' , color : 'white'}} key={items?.mal_id}>
-  <div className={styles.categoriesAnime} key={items?.mal_id}> 
-   <Image src={items?.images?.jpg?.image_url} height={250} width={250} className= {styles.categoriesAnimeImg} alt={data[start]?.title} />
-    <h2>{items?.title}</h2>
-  </div>
-    </Link>
-  ))}
 
-      </div>
  
 
       <Footer />
