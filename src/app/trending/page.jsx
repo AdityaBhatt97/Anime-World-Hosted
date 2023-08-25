5f6dc22507f8dff55a261414fada919c725f4870
Aditya Bhatt <bhattaditya92@gmail.com>
2023-08-25 12:40:22 +0530

    test host

--- src/app/trending/page.jsx
+++ src/app/trending/page.jsx
@@ -39,7 +39,9 @@ export  default async  function page(){
     // console.log(data[0])
     
 let start = 0
-let end = data.length;
+// let end = data?.length;
+let end = 25;
+// console.log(end)
 
 
 
