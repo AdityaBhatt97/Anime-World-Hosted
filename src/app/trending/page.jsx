1dd1b694bf29f09be95a13f964f87d08496b68b2
Aditya Bhatt <bhattaditya92@gmail.com>
2023-08-25 13:10:50 +0530

    cm

--- src/app/trending/page.jsx
+++ src/app/trending/page.jsx
@@ -6,9 +6,7 @@ import Link from "next/link"
 
 
 const getPopular = async() => {
-// const res = await fetch('https://api.jikan.moe/v4/top/anime')
-  const res = await fetch('https://api.jikan.moe/v4/recommendations/anime')
-
+const res = await fetch('https://api.jikan.moe/v4/top/anime')
 const data = await res.json()
 return data.data
 
@@ -35,14 +33,16 @@ export  default async  function page(){
 
 
 
-          <Link  href={`/anime/${data[0]?.entry[0]?.mal_id}`} style={{textDecoration : 'none' , color : 'white'}}>
+          <Link  href={`/anime/${data[0]?.mal_id}`} style={{textDecoration : 'none' , color : 'white'}}>
           <div className= {styles.popularAnime}> 
-         <Image src={data[0]?.entry[0]?.images?.webp?.large_image_url} alt={data[0]?.entry[0]?.title} height={600} width={600} className={styles.popularImages}/>
-          <h2>{data[0]?.entry[0]?.title}</h2>
+         <Image src={data[0]?.images?.webp?.large_image_url} alt={data[0]?.title} height={600} width={600} className={styles.popularImages}/>
+          <h2>{data[0]?.title}</h2>
           </div>
           </Link>
        
           
+     
+         
           
 
     
