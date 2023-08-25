8feddb9a8e4c649213de12081b299a9c0783530e
Aditya Bhatt <bhattaditya92@gmail.com>
2023-08-25 13:05:58 +0530

    check againnn

--- src/app/trending/page.jsx
+++ src/app/trending/page.jsx
@@ -1,76 +1,55 @@
-
 import { Navbar } from "@/app/components/Navbar"
-import styles from '../styles/categories.module.css'
+import styles from '../styles/popular.module.css'
 import Image from 'next/image'
 import Link from "next/link"
-import { Footer } from "@/app/components/Footer"
 
 
 
-
-
-const getGenres = async() => {
-
-  
-  const date = new Date();
-  const year = date.getFullYear();
-  const animeYear = year - 5;
-  
-  // const res = await fetch(`https://api.jikan.moe/v4/anime?genres=${param}&min_score=7&start_date=${animeYear}-01-01&sort=desc&sfw`)
-//   const res = await fetch(`https://api.jikan.moe/v4/anime?&min_score=7&start_date=${animeYear}-01-01&sort=desc`)
+const getPopular = async() => {
+// const res = await fetch('https://api.jikan.moe/v4/top/anime')
   const res = await fetch('https://api.jikan.moe/v4/recommendations/anime')
-  
-  const data = await res.json()
 
-  return data?.data
-  
-  
-  
-  }
+const data = await res.json()
+return data.data
 
+}
 
 
 export  default async  function page(){
 
 
-    const data = await getGenres()
-    
- 
-    // console.log(data[0])
-    
-let start = 0
-// let end = data?.length;
-let end = 25;
-// console.log(end)
 
+    const data = await getPopular()
+    // console.log(data)
 
+   
 
   return (
 
-    <div className={styles.categories}>
-      <Navbar />
-      
- 
+    <div className= {styles.popular} >
 
-{/* One */}
-<Link href={`/anime/${data[start]?.entry[start]?.mal_id}`} style={{textDecoration : 'none' , color : 'white'}}>
-  <div className={styles.categoriesTopAnimes}>
-   
-   
-   <Image src={data[start]?.entry[start]?.images?.webp?.large_image_url} height={500} width={500}  className={styles.mainImages} alt={data[start]?.entry[start]?.title}/>
-   {/* <div className= {styles.categoriesTopAnimeOneInfo}> */}
+<Navbar />
 
-  <h2 className={styles.categoriesTopTitle}>{data[start]?.entry[start]?.title}</h2>
-
-   {/* </div> */}
-   
-  </div>
-</Link>
+<div className={styles.popularAnimes}>
 
 
 
-      <Footer />
 
+          <Link  href={`/anime/${data[0]?.entry[0]?.mal_id}`} style={{textDecoration : 'none' , color : 'white'}}>
+          <div className= {styles.popularAnime}> 
+         <Image src={data[0]?.entry[0]?.images?.webp?.large_image_url} alt={data[0]?.entry[0]?.title} height={600} width={600} className={styles.popularImages}/>
+          <h2>{data[0]?.entry[0]?.title}</h2>
+          </div>
+          </Link>
+       
+          
+          
+
+    
+        </div>
+
+    
+  
     </div>
   )
 }
